084a56b8cccf4ba1fa7648e594aa380c68ab20eb
kimonjo67 <monjokim@gmail.com>
2018-02-25 22:20:51 -0600

    edited home,server, apiRoutes

---
+++ app/data/friends.js
@@ -0,0 +1,65 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: "Sam Mwangi",
+    photo:
+      "https://avatars3.githubusercontent.com/u/33467467?s=400&u=aa8843ef101d8708c71b181b73868e49f56bc245&v=4",
+    scores: ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
+  },
+  {
+    name: "simba",
+    photo:
+      "https://www.google.com/search?q=simba&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiE3JLyq8LZAhVqm-AKHYsgDuoQ_AUICigB&biw=1536&bih=759#imgrc=QApU_npYtubZSM:",
+    scores: ["2", "2", "4", "5", "3", "1", "2", "3", "1", "3"]
+  },
+  {
+    name: "Denzel",
+    photo:
+      "https://www.biography.com/.image/t_share/MTE1ODA0OTcxODE3OTI4MjA1/denzel-washington-9524687-2-402.jpg",
+    scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
+  },
+  {
+    name: "Pink Panther",
+    photo: "http://is2.mzstatic.com/image/thumb/Video/v4/e4/be/db/e4bedbbd-c698-891b-efd6-d901f7737fb5/source/1200x630bb.jpg",
+    scores: ["5", "1", "1", "5", "", "5", "3", "5", "5", "5"]
+  },
+  {
+    name: "Hulk Hogan",
+    photo:
+      "https://img.cinemablend.com/filter:scale/quill/d/a/6/7/d/a/da67da48db1e6c4866b7f2ca603e06cb09532d98.jpg?mw=600",
+    scores: ["5", "5", "3", "5", "5", "4", "5", "2", "5", "2"]
+  },
+  {
+    name: "Kevin",
+    photo:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BOTczNTg0YTItYTZiZi00YjNkLTgxZjUtMzQ0ZTEzYmMxN2YzXkEyXkFqcGdeQXVyNDgyODgxNjE@._V1_.jpg",
+    scores: ["5", "5", "4", "1", "3", "5", "5", "1", "5", "5"]
+  },
+  {
+    name: "Brian",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/en/1/12/Brian_Griffin.png",
+    scores: ["4", "5", "5", "4", "5", "5", "4", "5", "4", "5"]
+  },
+  {
+    name: "Draymond",
+    photo:
+      "https://cdn-images-1.medium.com/max/1200/1*KIvQ0Iy_zlYbh3HgKSieGQ.jpeg",
+    scores: ["5", "5", "3", "3", "5", "5", "3", "5", "5", "5"]
+  },
+  {
+    name: "Obama",
+    photo:
+      "http://cdn01.dailycaller.com/wp-content/uploads/2017/10/Obama-laughing-Getty-Images-Jewel-Samad-GOOD.jpg",
+    scores: ["2", "2", "2", "2", "2", "2", "2", "2", "2", "2"]
+  },
+  {
+    name: "Mark",
+    photo:
+      "https://specials-images.forbesimg.com/imageserve/59d5062131358e542c034eb7/416x416.jpg?background=000000&cropX1=419&cropX2=1409&cropY1=53&cropY2=1044",
+    scores: ["5", "5", "2", "5", "3", "5", "5", "5", "5", "5"]
+  }
+];
+
+module.exports = friendList;
